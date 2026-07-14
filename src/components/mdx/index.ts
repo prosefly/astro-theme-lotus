77cc23a36467cf6b736490e962ff649fcb443f7a
Hsiaoming Yang <me@lepture.com>
2026-07-14 20:01:50 +0900

    feat: add mdx docs components

---
+++ src/components/mdx/index.ts
@@ -0,0 +1,8 @@
+export { default as Badge } from './Badge.astro';
+export { default as Callout } from './Callout.astro';
+export { default as Card } from './Card.astro';
+export { default as CardGrid } from './CardGrid.astro';
+export { default as Step } from './Step.astro';
+export { default as Steps } from './Steps.astro';
+export { default as TabItem } from './TabItem.astro';
+export { default as Tabs } from './Tabs.astro';
