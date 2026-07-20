d70cd8432f2f3cc16673142826400db39a0e904a
Hsiaoming Yang <me@lepture.com>
2026-07-20 10:18:24 +0900

    feat: export Sidebar component

--- packages/astro-theme-lotus/src/components.ts
+++ packages/astro-theme-lotus/src/components.ts
@@ -20,8 +20,14 @@ export { default as Prose } from './components/theme/Prose.astro';
 export { default as SearchDialog } from './components/theme/SearchDialog.astro';
 export { default as SearchDialogContent } from './components/theme/SearchDialogContent.astro';
 export { default as SearchDialogTrigger } from './components/theme/SearchDialogTrigger.astro';
+export { default as Sidebar } from './components/theme/Sidebar.astro';
 export { default as SiteBrand } from './components/theme/SiteBrand.astro';
 export { default as ThemeModeButton } from './components/theme/ThemeModeButton.astro';
 export { default as ThemeModeSegmentedControl } from './components/theme/ThemeModeSegmentedControl.astro';
 export { default as ThemeModeSelect } from './components/theme/ThemeModeSelect.astro';
 export { default as ThemeModeSwitch } from './components/theme/ThemeModeSwitch.astro';
+export type {
+  DocsSidebarGroupNav,
+  DocsSidebarItem,
+  DocsSidebarNav,
+} from './lib/docs';
